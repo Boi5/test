c40b9faf967f6195189a7e9fd988042de1694a83
Boi5 <59049515+Boi5@users.noreply.github.com>
2023-09-28 20:30:36 +0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -13,7 +13,7 @@ my_id: int = 1433639923
 channels_ids= [ 'telegram', 'Alarabiya', 'BBCArabic']
 
 
-phone_number = '+963998174915'
+phone_number = '+' 
 
 client = TelegramClient('anon', api_id=api_id, api_hash=api_hash)
 
